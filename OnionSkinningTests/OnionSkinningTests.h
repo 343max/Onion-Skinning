665ab8fb9e2f9903a1915e6ceb87a13d335f3174
Max Winde <max.winde@gmail.com>
2011-09-14 17:23:32 +0200

    Initial Commit

---
+++ OnionSkinningTests/OnionSkinningTests.h
@@ -0,0 +1,13 @@
+//
+//  OnionSkinningTests.h
+//  OnionSkinningTests
+//
+//  Created by Max Winde on 14.09.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OnionSkinningTests : SenTestCase
+
+@end
---
+++ OnionSkinningTests/OnionSkinningTests.m
@@ -0,0 +1,32 @@
+//
+//  OnionSkinningTests.m
+//  OnionSkinningTests
+//
+//  Created by Max Winde on 14.09.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "OnionSkinningTests.h"
+
+@implementation OnionSkinningTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OnionSkinningTests");
+}
+
+@end
